7c721a837cb5bb904d74c39a07817dcadc5d5375
huazai <1461522031@qq.com>
2025-01-21 15:17:07 +0800

    属性列表查询，组件抽取

---
+++ src/views/modules/common/base-divider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="base-divider">
+    <div class="divider-line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseDivider'
+}
+</script>
+
+<style scoped>
+.base-divider {
+  display: flex;
+  align-items: center;
+  padding: 12px;
+}
+
+.divider-line {
+  flex-grow: 1;
+  border-top: 1px solid #ccc;
+  height: 0;
+}
+</style>
